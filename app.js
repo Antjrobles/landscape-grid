2a2cd64679615af7f12b4f0ba7dde09e6fd3ec73
Antonio Robles <51507400+Antjrobles@users.noreply.github.com>
2023-06-03 01:19:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,27 +5,42 @@ const searchBtn = document.querySelector('.searchBtn');
 const searchBar = document.querySelector('.searchBar');
 const searchInput = document.getElementById('searchInput');
 const searchClose = document.getElementById('searchClose');
-const inputField = document.getElementById('searchInput')
+const inputField = document.getElementById('searchInput');
+
+const select = document.querySelector('.hidden');
+const selectBtn = document.getElementById('btn-select');
+const numberInput = document.getElementById('desired-number');
 
 
-async function fetchImages() {
+// GET THE DESIRED NUMBER
+function getSelectedNumber() {
+  const desiredNumber = numberInput.value;
+  fetchImages(desiredNumber);
+};
+
+
+async function fetchImages(desiredNumber) {
+  select.style.display = 'block';
+  select.classList.add('showSelect');
+
   cardsContainer.innerHTML = ''; // Limpiar el contenido existente
-  const perPage = 5;
+  const perPage = desiredNumber ;
+  console.log(perPage);
   // const searchParameter = inputText;
-  
+
   try {
     const response = await fetch(
       `https://api.pexels.com/v1/search?query=landscape&per_page=${perPage}`,
       {
         headers: {
-          Authorization: 'F5RU35VJ0hiSHmLcbiwhy9S5IjzIyJuDWByJRt2kwXs6qEY31oE9i955',
+          Authorization:
+            'F5RU35VJ0hiSHmLcbiwhy9S5IjzIyJuDWByJRt2kwXs6qEY31oE9i955',
         },
       }
     );
     const data = await response.json();
     const photos = data.photos;
     shuffle(photos);
-
 
     photos.forEach((photo) => {
       const card = createCard(photo);
@@ -57,11 +72,16 @@ function createCard(photo) {
   const author = document.createElement('author');
   author.innerText = photo.photographer;
 
-  author.innerHTML = author.innerHTML === 'Pixabay' ? 'Stefan Keller' :
-                  author.innerHTML === 'Nextvoyage' ? 'Antonio Robles' :
-                  author.innerHTML === 'katja' ? 'Katja Salenko' :
-                  author.innerHTML === 'Baskin Creative Studios' ? 'Mario Baskin' :
-                  author.innerHTML;
+  author.innerHTML =
+    author.innerHTML === 'Pixabay'
+      ? 'Stefan Keller'
+      : author.innerHTML === 'Nextvoyage'
+      ? 'Antonio Robles'
+      : author.innerHTML === 'katja'
+      ? 'Katja Salenko'
+      : author.innerHTML === 'Baskin Creative Studios'
+      ? 'Mario Baskin'
+      : author.innerHTML;
 
   text.appendChild(author);
   card.appendChild(front);
@@ -75,22 +95,19 @@ function createCard(photo) {
 }
 
 // SEARCH BAR
-searchBtn.addEventListener('click', function() {
+searchBtn.addEventListener('click', function () {
   searchBar.style.visibility = 'visible';
-  searchBtn.style.visibility = 'hidden'; 
+  searchBtn.style.visibility = 'hidden';
   searchBar.classList.add('open');
 });
 
-
-
-searchClose.addEventListener('click', function() {
+searchClose.addEventListener('click', function () {
   searchBtn.style.visibility = 'visible';
   searchBar.style.visibility = 'hidden';
   searchBar.classList.remove('open');
- });
+});
 
-
- // SHUFFLE FOTOS
+// SHUFFLE FOTOS
 function shuffle(array) {
   let currentIndex = array.length,
     randomIndex;
@@ -99,43 +116,36 @@ function shuffle(array) {
     currentIndex--;
     [array[currentIndex], array[randomIndex]] = [
       array[randomIndex],
-      array[currentIndex]
+      array[currentIndex],
     ];
   }
   return array;
 }
 
-
 // SEARCH INPUT
 inputField.addEventListener('keyup', (e) => {
   const inputText = e.target.value;
-  fetchSearchImages(inputText);  
-})
-
-btnEl.addEventListener('click', fetchImages);
-
-
-
-
+  fetchSearchImages(inputText);
+});
 
 //fetchSearchImages
 async function fetchSearchImages(inputText) {
   cardsContainer.innerHTML = ''; // Limpiar el contenido existente
   const perPage = 5;
   const searchParameter = inputText;
-  
+
   try {
     const response = await fetch(
       `https://api.pexels.com/v1/search?query=${searchParameter}&per_page=${perPage}`,
       {
         headers: {
-          Authorization: 'F5RU35VJ0hiSHmLcbiwhy9S5IjzIyJuDWByJRt2kwXs6qEY31oE9i955',
+          Authorization:
+            'F5RU35VJ0hiSHmLcbiwhy9S5IjzIyJuDWByJRt2kwXs6qEY31oE9i955',
         },
       }
     );
     const data = await response.json();
     const photos = data.photos;
-
 
     photos.forEach((photo) => {
       const card = createCard(photo);
@@ -167,11 +177,16 @@ function createCard(photo) {
   const author = document.createElement('author');
   author.innerText = photo.photographer;
 
-  author.innerHTML = author.innerHTML === 'Pixabay' ? 'Stefan Keller' :
-                  author.innerHTML === 'Nextvoyage' ? 'Antonio Robles' :
-                  author.innerHTML === 'katja' ? 'Katja Salenko' :
-                  author.innerHTML === 'Baskin Creative Studios' ? 'Mario Baskin' :
-                  author.innerHTML;
+  author.innerHTML =
+    author.innerHTML === 'Pixabay'
+      ? 'Stefan Keller'
+      : author.innerHTML === 'Nextvoyage'
+      ? 'Antonio Robles'
+      : author.innerHTML === 'katja'
+      ? 'Katja Salenko'
+      : author.innerHTML === 'Baskin Creative Studios'
+      ? 'Mario Baskin'
+      : author.innerHTML;
 
   text.appendChild(author);
   card.appendChild(front);
@@ -183,3 +198,11 @@ function createCard(photo) {
 
   return card;
 }
+
+
+
+
+btnEl.addEventListener('click', function() {
+  const desiredNumber = numberInput.value;
+  fetchImages(desiredNumber)
+});
